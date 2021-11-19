82c7869b7054d0b636fe377a0cb523a36975e585
nadhifrmdhn <nadhifrmdhn@gmail.com>
2021-11-20 01:06:57 +0700

    penambahan di file nasgor.py

--- nasgor.py
+++ nasgor.py
@@ -1,18 +1,21 @@
 toppings = {
   'sosis': 1000,
-  'ati_ampela': 2000,
+  'ati ampela': 2000,
   'telur ceplok': 2000,
   'telur dadar': 2000,
   'teri': 3000,
   'bakso': 3000,
   'seafood': 4000
+  'keju mozarella': 5000,
+  'karage': 6000,
+  'sayuran': 3000
 }
 
 porsi = [
   'diet',
   'sedang',
   'besar',
-  'hajatan'
+  'catering'
 ]
 
 pedas = [5, 10, 15, 20]
